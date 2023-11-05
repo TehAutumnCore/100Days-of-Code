d6f139009f64a7487225c2a0d67c89370dee31a0
TehAutumnCore <naruto13581@gmail.com>
2023-11-04 20:55:13 -0700

    Day 3 of 100 Days of Code

---
+++ Day 3/Conditional.py
@@ -0,0 +1,12 @@
+# if condition:
+#     do this
+# else:
+#     do this
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
