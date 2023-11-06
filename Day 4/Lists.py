31ed696dd4652144908d12ed3f11680c00fc9e95
TehAutumnCore <naruto13581@gmail.com>
2023-11-05 18:05:33 -0800

    Day 4 of 100 Days of Code

---
+++ Day 4/Lists.py
@@ -0,0 +1,7 @@
+#Data structure: Lists
+
+states_of_america = ["MA","RI","CT","NH","ME"]
+states_of_america[1] = "MD"
+states_of_america.append("NY")
+print(states_of_america[1])
+print(states_of_america[-1])
---
+++ Day 4/random_mod.py
@@ -0,0 +1,14 @@
+import random
+import my_module
+
+# random_integer = random.randint(1, 10)  # Use randint instead of random_integer
+# print(random_integer)
+
+# print(my_module.pi)
+
+# 0.000000-0.99999...
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")
