69d1ddb2a0737517dccf50a752424e3633716cf3
TehAutumnCore <naruto13581@gmail.com>
2023-11-06 11:17:11 -0800

    Day 4 of 100 Days of Code

--- Day 4/Lists.py
+++ Day 4/Lists.py
@@ -1,7 +1,17 @@
-#Data structure: Lists
+# #Data structure: Lists
+#
+# states_of_america = ["MA","RI","CT","NH","ME"]
+# states_of_america[1] = "MD"
+# states_of_america.append("NY")
+# print(states_of_america[1])
+# print(states_of_america[-1])
+# print(len(states_of_america))
 
-states_of_america = ["MA","RI","CT","NH","ME"]
-states_of_america[1] = "MD"
-states_of_america.append("NY")
-print(states_of_america[1])
-print(states_of_america[-1])
+dirty_dozen = ["Strawberries","Spinach","Kale","Nectarines","Apples","Grapes","Peaches",
+               "Cherries","Tomatoes","Celery","Potatoes"]
+
+fruits = ["Strawberries","Nectarines","Apples","Grapes","Peaches","Cherries","Pears"]
+vegetables = ["Spinach","Kale","Tomatoes","Celery","Potatoes"]
+
+dirty_dozen = [fruits,vegetables]
+print(dirty_dozen)
