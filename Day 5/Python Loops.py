d1bac628c142fda6272317a41c568aabf8d533f1
TehAutumnCore <naruto13581@gmail.com>
2023-11-06 19:55:06 -0800

    Day 5 of 100 Days of Code

--- Day 5/Python Loops.py
+++ Day 5/Python Loops.py
@@ -1,1 +1,11 @@
 #Python Loops
+
+#For Loop
+# for item in list_of_items:
+    #do something to each item
+
+fruits = ["Apple","Peach","Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
