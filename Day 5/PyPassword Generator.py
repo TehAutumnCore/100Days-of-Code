7240dc1380b95e109091f0e514b0362fe2588d03
Gary Rojas <naruto13581@gmail.com>
2023-11-11 18:58:28 -0700

    Day 5 of 100 Days of Code

--- Day 5/PyPassword Generator.py
+++ Day 5/PyPassword Generator.py
@@ -1,1 +1,19 @@
-#Welcome to a PyPassword Generator
+#Welcome to the PyPassword Generator
+import random
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+           'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+numbers = ['0','1','2','3','4','5','6','7','8','9']
+symbols = ['!','@','#','$','%','^','&','*','(',')']
+print('Welcome to a PyPassword Generator')
+letters_wanted = int(input("How many letters would you like in your password? \n"))
+symbols_wanted = int(input("How many symbols would you like? \n"))
+numbers_wanted = int(input("How many numbers would you like? \n"))
+
+random_letters = random.sample(letters,letters_wanted)
+random_numbers = random.sample(numbers,numbers_wanted)
+random_symbols = random.sample(symbols,symbols_wanted)
+random_combined = random_letters + random_numbers + random_symbols
+random.shuffle(random_combined)
+password = ''.join(random_combined)
+
+print(f"Here is your password: {password}")
---
+++ Day 5/for loops and the range() function.py
@@ -0,0 +1,11 @@
+#For Loop with lists
+# fruits = ["Apple","Peach","Pear",]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + "Pie")
+
+#For Loop in Range
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
