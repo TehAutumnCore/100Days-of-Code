eda842af14a30361fd97274883d75418f14bd61e
Gary Rojas <naruto13581@gmail.com>
2023-11-17 12:00:19 -0700

    Day 7 of 100 Days of Code

--- Day 7/Hangman Project.py
+++ Day 7/Hangman Project.py
@@ -1,8 +1,71 @@
-#Step 3
+#Step 4
 
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+end_of_game = False
 world_list = ["ardvark","baboon","camel"]
 chosen_word = random.choice(world_list)
+word_length = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#Set 'lives' to equal 6.
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -12,20 +75,29 @@ display = []
 word_length = len(chosen_word)
 for _ in range(word_length):          # or for _ in range(len(chosen_word)):
     display += "_"            #or display += "_"
-print(display)
 
-#TODO-1: - Use a while loop to let the user guess again
-# The loop should only stop once the user has guessed
-# all the letters in the chosen_word and 'display'
-# has no more blanks ("_"). Then you can tell the user they've won
+while not end_of_game:       #or while end_of_game is == to False
+    guess = input("Guess a letter: ").lower()
 
-guess = input("Guess a letter: ").lower()
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter} \n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
-#Check guessed letter
-for position in range(word_length):
-    letter = chosen_word[position]
-    print(f"Current position: {position}\n Current letter: {letter} \n Guessed letter: {guess}")
-    if letter == guess:
-        display[position] = letter
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #then reduce 'lives' by 1.
+    #if lives goes down to 0 then the game should stop and it should print "You lose."
+    if letter
 
-print(display)
+
+    #join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You Win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
