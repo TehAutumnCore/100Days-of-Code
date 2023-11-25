fc1165a6c05a996c75ae5b1622f03740f11d5834
Gary Rojas <naruto13581@gmail.com>
2023-11-25 10:34:22 -0700

    Day 8 of 100 Days of Code

--- Day 8/Caesar Cipher.py
+++ Day 8/Caesar Cipher.py
@@ -7,7 +7,6 @@ shift = int(input("Type the shift number:\n"))
 
 #Don't change code above ^
 
-#TODO-1: Create a function called encrypt that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
@@ -16,13 +15,25 @@ def encrypt(plain_text, shift_amount):
         new_letter = alphabet[new_position]
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forward in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(cipher_text, shift_amount):
+    #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by theh shift amount and print the decrypted text.
+    decipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        decipher_text += new_letter
+    print(f"The decoded text is {decipher_text}")
+    #cipher_text = "mjqqt
+    #shift = 5
+    # plain_text = "hello"
+    #print output: "The decoded text is hello"
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable.
+#TODO-3: Then call the correct function based on that 'direction' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+
 
 encrypt(plain_text= text, shift_amount=shift)
+decrypt(plain_text=text, shift_amount=shift)
