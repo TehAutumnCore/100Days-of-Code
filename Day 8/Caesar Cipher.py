95a8e43ebb09858388c22f5eae4a8792f02dc85c
Gary Rojas <naruto13581@gmail.com>
2023-11-27 11:41:17 -0700

    Day 8 of 100 Days of Code

--- Day 8/Caesar Cipher.py
+++ Day 8/Caesar Cipher.py
@@ -16,32 +16,6 @@ def caeser(start_text,shift_amount, cipher_direction):
         new_position = position + shift_amount
         end_text += alphabet[new_position]
     print(f"The {cipher_direction}d text is {end_text}")
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
-    #cipher_text = "mjqqt
-    #shift = 5
-    # plain_text = "hello"
-    #print output: "The decoded text is hello"
-
-if direction == "encode":
-    encrypt(plain_text= text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(plain_text=text, shift_amount=shift)
 
 #TODO-2:Call the caesar() function, passing over the 'text', 'shift', and 'direction' values.
 
